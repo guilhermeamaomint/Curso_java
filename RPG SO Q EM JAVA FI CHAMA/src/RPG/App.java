d728f827ef94a253670e4cb5812cfa7ace3d6e4a
guilhermegit <g360guilherme@gmail.com>
2024-02-22 04:30:16 -0300

    first commit

---
+++ RPG SO Q EM JAVA FI CHAMA/src/RPG/App.java
@@ -0,0 +1,31 @@
+package RPG;
+
+public class App {
+    public static void main(String[] args){
+        int x = 0;
+        Ficha Iccarion = new Ficha();
+        Iccarion.forca = "20";
+        Iccarion.constituicao = "12";
+        Iccarion.destreza = "16";
+        Iccarion.inteligencia = "10";
+        Iccarion.sabedoria = "8";
+        Iccarion.carisma = "15";
+        Iccarion.vidaBase = "154";
+        Iccarion.proeficiencia = "5";
+        Iccarion.classeDeArmadura = "19";
+        Iccarion.iniciativa = "1";
+
+        Ficha Skamos = new Ficha();
+        Skamos.forca = "8";
+        Skamos.constituicao = "14";
+        Skamos.destreza = "13";
+        Skamos.inteligencia = "11";
+        Skamos.sabedoria = "13";
+        Skamos.carisma = "18";
+        Skamos.vidaBase = "26";
+        Skamos.proeficiencia = "3";
+        Skamos.classeDeArmadura = "17";
+        Skamos.iniciativa = "2";
+
+    }
+}
---
+++ RPG SO Q EM JAVA FI CHAMA/src/RPG/Ficha.java
@@ -0,0 +1,15 @@
+package RPG;
+public class Ficha {
+    //to me cagando ja termino saporra
+    //cheguei
+    public String forca;
+    public String constituicao;
+    public String destreza;
+    public String inteligencia;
+    public String sabedoria;
+    public String carisma;
+    public String vidaBase;
+    public String proeficiencia;
+    public String classeDeArmadura;
+    public String iniciativa;
+}
